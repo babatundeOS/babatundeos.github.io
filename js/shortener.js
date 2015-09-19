438f5fba2f9e35f427d0beb5355c1e814f68ffdd
mcwaloy <babatundeoyeyemi@gmail.com>
2015-09-19 22:07:41 +0700

    Commit from office

--- js/shortener.js
+++ js/shortener.js
@@ -1,33 +1,14 @@
-$(document).ready(function() {
-	var showChar = 100;
-	var ellipsestext = "...";
-	var moretext = "more";
-	var lesstext = "less";
-	$('.more').each(function() {
-		var content = $(this).html();
+/* Used to shorten the About Us section */
 
-		if(content.length > showChar) {
-
-			var c = content.substr(0, showChar);
-			var h = content.substr(showChar-1, content.length - showChar);
-
-			var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
-
-			$(this).html(html);
+function showHide(shID) {
+	if (document.getElementById(shID)) {
+		if (document.getElementById(shID+'-show').style.display != 'none') {
+			document.getElementById(shID+'-show').style.display = 'none';
+			document.getElementById(shID).style.display = 'block';
 		}
-
-	});
-
-	$(".morelink").click(function(){
-		if($(this).hasClass("less")) {
-			$(this).removeClass("less");
-			$(this).html(moretext);
-		} else {
-			$(this).addClass("less");
-			$(this).html(lesstext);
+		else {
+			document.getElementById(shID+'-show').style.display = 'inline';
+			document.getElementById(shID).style.display = 'none';
 		}
-		$(this).parent().prev().toggle();
-		$(this).prev().toggle();
-		return false;
-	});
-});
+	}
+}
